dfb5a6a85e8171f5b640cb33e861e57dd16ddd7f
Si Hao <tangsihao1998@gmail.com>
2019-06-13 11:44:24 +0700

    Add new Footer for Website

--- src/component/footer/footer.js
+++ src/component/footer/footer.js
@@ -9,13 +9,38 @@ class footer extends Component {
                 <div className="footer1">
                     <div><img className="Logo" src="images/Logo/logo.png" srcSet="images/Logo/logo@2x.png 2x,images/Logo/logo@3x.png 3x" 
                                     alt="Company's Logo"></img></div>
-                    <div></div>
-                    <div></div>
+                    <div className="footer-menu">
+                        {/* Component anchor */}
+                        <div>Home</div>
+                        <div>Products</div>
+                        <div>Services</div>
+                        <div>About Us</div>
+                        <div>Helps</div>
+                        <div>Contacts</div>
+                    </div>
+                    <div>
+                        <a className="social-icon" href="https://twitter.com/?lang=vi"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+                        <a className="social-icon" href="https://www.facebook.com/"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+                        <a className="social-icon" href="https://www.instagram.com/"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+                    </div>
                 </div>
                 {/* Privacy & Options */}
                 <div className="footer2">
-                    <div>asdsadsadsadas</div>
-                    <div></div>
+                    {/* Component footermenu */}
+                    <div className="footer-menu">
+                        {/* Component anchor */}
+                        <div>Home</div>
+                        <div>Products</div>
+                        <div>Services</div>
+                        <div>About Us</div>
+                        <div>Helps</div>
+                        <div>Contacts</div>
+                    </div>
+                    <div className="policy">
+                        {/* Component anchor */}
+                        <div>Privacy Policy</div>
+                        <div>Term & Conditions</div>
+                    </div>
                 </div>
             </div>
         )
